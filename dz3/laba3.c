e6678e91ce4d948a39c87c38e0bb034b7c76c431
Voverevc <114072997+Voverevc@users.noreply.github.com>
2024-09-22 22:39:08 +0300

    Add files via upload

---
+++ dz3/laba3.c
@@ -0,0 +1,16 @@
+﻿
+#include <locale.h>
+#include <stdio.h>
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int a;
+	int b;
+	float del;
+	printf("Введите число 1:");
+	scanf("%d", &a);
+	printf("Введите число 2:");
+	scanf("%d", &b);
+	del = (float)a / b;
+	printf("+ = %d, - = %d, * = %d, / = %f", a + b, a - b, a * b, del);
+	getchar();
+}
