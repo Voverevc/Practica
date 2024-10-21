589cbe224b82c896008c60fa805ac07d75a944b2
Voverevc <114072997+Voverevc@users.noreply.github.com>
2024-10-21 13:05:21 +0300

    Add files via upload

---
+++ Dz5/DZ5.c
@@ -0,0 +1,19 @@
+#include<locale.h>
+#include<stdio.h>
+#pragma warning(disable :4996)
+#include<math.h>
+#define _USE_MATH_DEFINES
+#define M_PI 3.14159265358979323846
+#define a 7.10*10^-9
+float funcia(float x, float y) {
+	float otr = ((pow))
+}
+int main() {
+	float x, y;
+	printf("¬ведите x :");
+	scanf("%lf", &x);
+	printf("¬ведите y :");
+	scanf("%lf", &y);
+	
+
+}
